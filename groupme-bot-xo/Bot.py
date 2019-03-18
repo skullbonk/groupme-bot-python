6c588bf498b5b8f3f857af55976160279dceadce
Skullbonk <reagan.fairbanks@gmail.com>
2019-03-18 13:29:56 -0600

    i think i might have a bot that works

--- groupme-bot-xo/Bot.py
+++ groupme-bot-xo/Bot.py
@@ -4,6 +4,25 @@ Created on Mar 15, 2019
 @author: rfai3591
 '''
 
-if __name__ == '__main__':
-    pass
-print("buns");
+import requests
+import time
+
+request_params = {'token': 'asPGAP0QNdGPnsDC8yoOb0uryWzHqzybrsOCF8nn'}
+
+while True:
+    response = requests.get('https://api.groupme.com/v3/groups/35396592/messages', params = request_params)
+    
+    if (response.status_code == 200):
+        response_messages = response.json()['response']['messages']
+    
+        for message in response_messages:
+            print(message['text'])
+            if ('xo' in message):
+                botResponse = 'oi piss off'
+                
+                # send response to the group
+                post_params = {'bot_id' : '9ac5c52ec5efaee1bce225eb92', 'text' : botResponse}
+                requests.post('https://api.groupme.com/v3/bots/post', params = post_params)
+                request_params['since_id'] = message['id']
+                break
+    time.sleep(5)
