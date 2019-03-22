63f9b90e8b6304d46528b29050c878589ea0f21b
skullbonk <32400834+skullbonk@users.noreply.github.com>
2019-03-21 21:18:56 -0600

    it reads names fine but it needs to write them on a new line

--- groupme-bot-xo/Bot.py
+++ groupme-bot-xo/Bot.py
@@ -8,75 +8,108 @@ import string
 import requests
 import time
 import random
+import fileinput
 
 request_params = {'token': 'asPGAP0QNdGPnsDC8yoOb0uryWzHqzybrsOCF8nn'}
 member_list = {'chonk', 'Dillon Foster', 'Gayhanderson', 'LABE', 'Rillo notPillo', 'Sack', 'Zo'}
 
-def analyze_message(received_message) -> str:
+
+def analyze_message(sender, received_message) -> str:
     received_message = received_message.casefold()
     generic_responses = ['m hahahahahahaha', 'aRe yOu GuyS ChAtTiNG wiThOuT mEeE???', 'friggin city slicker']
+    tagged_user = ''
+    full_response = ''
 
-    if 'fake' in received_message:
-        if 'not fake' in received_message or 'isn\'t fake' in received_message:
-            full_response = 'yeye'
-        else:
-            full_response = 'bet'
-    elif 'car' in received_message:
-        full_response = 'i used to have a car, but i crashed it on the information highway'
-    else:
-        full_response = random.choice(generic_responses)
-        
-    return full_response
-
-def build_bot_response(sender, received_message) -> str:
-    received_message = received_message.casefold()
-    bot_response = ''
     if 'xo' in received_message:
-        bot_response = '@' + sender + ' '
-        bot_response += analyze_message(received_message)
-    return bot_response
+        tagged_user = '@' + get_nickname(sender) + ' '
 
-def unsolicited_interjections(received_message):
-    received_message = received_message.casefold()
-    interjection = ''
-    if 'smash' in received_message:
-        interjection = 'I\'ll have you know, my wii fit is ranked'
+        if 'fake' in received_message:
+            if 'not fake' in received_message or 'isn\'t fake' in received_message:
+                full_response = 'yeye'
+            else:
+                full_response = 'bet'
+        elif 'car' in received_message:
+            full_response = 'i used to have a car, but i crashed it on the information highway'
 
-    if interjection:
-        post_params = {'bot_id': '9ac5c52ec5efaee1bce225eb92', 'text': interjection}
-        requests.post('https://api.groupme.com/v3/bots/post', params=post_params)
-        request_params['since_id'] = message['id']
+        if 'nickname' in received_message:
+            for member in member_list:
+                if member in received_message:
+                    name_to_nick = member
+                else:
+                    name_to_nick = sender
 
-def add_nickname(sender):
+            if 'change' or 'set' or 'give' or 'new' in received_message:
+                full_response = add_nickname(name_to_nick)
+            elif 'what' in received_message:
+                for member in member_list:
+                    if member in received_message:
+                        name_to_nick = member
+                    else:
+                        name_to_nick = sender
+                full_response = get_nickname(name_to_nick)
+        else:
+            full_response = random.choice(generic_responses)
+    else:
+        if 'smash' in received_message:
+            full_response = 'I\'ll have you know, my wii fit is ranked'
 
-    nicknames_file = open('nicknames.txt', 'r+')
-    nicknames_string = nicknames_file.read()
-    print(nicknames_file.read())
-    if sender in nicknames_string:
+        if 'hang' in received_message:
+            hang_responses = ['jazz game', 'effing snales', 'gimme 20', 'i\'ll be there in an hour and a half', 'nah i can\'t hang i have weener stuff to be doing instead']
+            full_response = random.choice(hang_responses)
+
+    return tagged_user + full_response
+
+
+def add_nickname(sender) -> str:
+    nick = ''
+    with open('nicknames.txt', 'r+') as nicknames_file:
+        nicknames_string = nicknames_file.read()
+        print(nicknames_file.read())
+        if sender in nicknames_string:
+            for line in nicknames_file:
+                if sender in line:
+                    nick = generate_nickname(sender)
+                    line = sender + '- ' + nick
+                    nicknames_file.writeline()
+        else:
+            nick = generate_nickname(sender)
+            nicknames_file.write(sender + '-' + nick)
+        print(nick)
+    return nick
+
+
+
+def get_nickname(sender) -> str:
+    with open('nicknames.txt', 'r') as nicknames_file:
         for line in nicknames_file:
             if sender in line:
-                string.replace(line.index)
-        string.replace()
+                return line.split('-', 1)[1]
+        else:
+            return sender
 
 
-def generate_nickname(sender) -> str:
+
+def generate_nickname(name_to_nick) -> str:
     nick_bank_1 = ['Bingle', 'Long', 'Fink', 'Chunk', 'Nut', 'Gonk', 'Lorkus', 'Chungus', 'Bungus', 'Dungus']
     nick_bank_2 = ['ton', 'bun', 'doink', 'dorf', 'florf', 'stein', 'heiny', 'beef', 'wink', 'boi', 'boy', 'town', 'man', 'guy']
-    if sender == 'chonk':
-        prefix = random.choice(['c', 'ch', 'cho', 'chon', 'chonk', 'cheese'])
-    elif sender == 'Dillon Foster':
-        prefix = random.choice(['Dill', 'Chilly', 'Fos', 'Dillon'])
-    elif sender == 'LABE':
-        prefix = random.choice(['La', 'Lab', 'Labe'])
-    elif sender == 'Rillo notPillo':
-        prefix = random.choice(['Rill', 'Pill', 'Rillo', 'Pillo', 'not', 'Rop', 'Pop'])
-    elif sender == 'Sack':
-        prefix = random.choice(['Sack', 'S', 'Sacc', 'Snack', 'Snacc'])
-    elif sender == 'Gayhanderson':
-        prefix = random.choice(['Goy', 'Hend', 'Guy', 'Gay', 'Hender', 'Hendz'])
+    sender_bank = ['bum']
+    if name_to_nick == 'chonk':
+        sender_bank = ['c', 'ch', 'cho', 'chon', 'chonk', 'cheese']
+    if name_to_nick == 'Dillon Foster':
+        sender_bank = ['Dill', 'Chilly', 'Fos', 'Dillon']
+    if name_to_nick == 'LABE':
+        sender_bank = ['La', 'Lab', 'Labe']
+    if name_to_nick == 'Rillo notPillo':
+        sender_bank = ['Rill', 'Pill', 'Rillo', 'Pillo', 'not', 'Rop', 'Pop']
+    if name_to_nick == 'Sack':
+        sender_bank = ['Sack', 'Sacc', 'Snack', 'Snacc']
+    if name_to_nick == 'Gayhanderson':
+        sender_bank = ['Goy', 'Hend', 'Guy', 'Gay', 'Hender', 'Hendz']
 
+    prefix = random.choice(sender_bank)
     final_nickname = prefix + random.choice(nick_bank_1) + random.choice(nick_bank_2)
     return final_nickname
+
 
 while True:
     response_page = requests.get('https://api.groupme.com/v3/groups/35396592/messages', params=request_params)
@@ -86,21 +119,15 @@ while True:
             message_sender = message['name']
             latest_message = message['text']
             print(message['name'], ': ', message['text'])
-            unsolicited_interjections(latest_message)
 
-            if 'change' and 'nickname' in latest_message:
-                add_nickname(message_sender)
-            
             if message_sender in member_list:
-                
-                to_send = build_bot_response(message_sender, latest_message)
+                to_send = analyze_message(message_sender, latest_message)
                 if to_send:
                     # send response to the group
                     post_params = {'bot_id': '9ac5c52ec5efaee1bce225eb92', 'text': to_send}
                     requests.post('https://api.groupme.com/v3/bots/post', params=post_params)
                     request_params['since_id'] = message['id']
                     break
-    
     time.sleep(5)
 
 
