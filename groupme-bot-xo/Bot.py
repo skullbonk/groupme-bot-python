62488364cdcf1b1220078d3c14d35a8e337c2390
Skullbonk <reagan.fairbanks@gmail.com>
2019-03-25 13:20:54 -0600

    added a separate text file for each member's nickname

--- groupme-bot-xo/Bot.py
+++ groupme-bot-xo/Bot.py
@@ -14,77 +14,71 @@ request_params = {'token': 'asPGAP0QNdGPnsDC8yoOb0uryWzHqzybrsOCF8nn'}
 member_list = {'chonk', 'Dillon Foster', 'Gayhanderson', 'LABE', 'Rillo notPillo', 'Sack', 'Zo'}
 
 
-def analyze_message(sender, received_message) -> str:
+def analyze_message(sender, received_message, message_attachment) -> str:
     received_message = received_message.casefold()
-    generic_responses = ['m hahahaha', 'aRe yOu GuyS ChAtTiNG wiThOuT mEeE???', 'friggin city slicker', 'ok why would you say that to me', 'it doesn\'t matter', 'are you sure about that']
+    generic_responses = ['m', 'aRe yOu GuyS ChAtTiNG wiThOuT mEeE???', 'friggin city slicker', 'ok why would you say that to me', 'it doesn\'t matter', 'are you sure about that', 'don\'t']
     tagged_user = ''
     full_response = ''
 
-    if 'xo' in received_message:
-        tagged_user = '@' + get_nickname(sender) + ' '
-        incl_tag = True
-        if 'fake' in received_message:
-            if 'not fake' in received_message or 'isn\'t fake' in received_message:
-                full_response = tagged_user + 'yeye'
-            else:
-                full_response = tagged_user + 'bet'
-
-        if 'nickname' in received_message:
-            incl_tag = False
-            for member in member_list:
-                if member in received_message:
-                    name_to_nick = member
+    if received_message:
+        if 'xo' in received_message:
+            tagged_user = '@' + get_nickname(sender) + ' '
+            incl_tag = True
+            if 'fake' in received_message:
+                if 'not fake' in received_message or 'isn\'t fake' in received_message:
+                    full_response = tagged_user + 'yeye'
                 else:
-                    name_to_nick = sender
+                    full_response = tagged_user + 'bet'
 
-            if 'change' or 'set' or 'give' or 'new' in received_message:
-                full_response = name_to_nick + '\'s nickname is now ' + add_nickname(name_to_nick)
+            if 'nickname' in received_message:
+                incl_tag = False
+                for member in member_list:
+                    if member in received_message:
+                        name_to_nick = member
+                    else:
+                        name_to_nick = sender
 
-            elif 'what' and not 'new' in received_message:
-                full_response = name_to_nick + '\'s nickname is ' + get_nickname(name_to_nick)
+                if 'change' or 'set' or 'give' or 'new' in received_message:
+                    full_response = name_to_nick + '\'s nickname is now ' + add_nickname(name_to_nick)
+
+                elif 'what' and not 'new' in received_message:
+                    full_response = name_to_nick + '\'s nickname is ' + get_nickname(name_to_nick)
+
+            else:
+                full_response = random.choice(generic_responses)
 
         else:
-            full_response = random.choice(generic_responses)
+            incl_tag = False
+            if 'smash' in received_message:
+                smash_responses = ['I\'ll have you know, my wii fit is ranked', 'melee bad', 'i only play brawl', 'you don\'t even know how to wavedash']
+                full_response = random.choice(smash_responses)
 
-    else:
-        incl_tag = False
-        if 'smash' in received_message:
-            smash_responses = ['I\'ll have you know, my wii fit is ranked', 'melee bad', 'i only play brawl', 'you don\'t even know how to wavedash']
-            full_response = random.choice(smash_responses)
+            if 'hang' in received_message:
+                hang_responses = ['jazz game', 'effing snales', 'gimme 20', 'i\'ll be there in an hour and a half', 'nah i can\'t hang i have weener stuff to be doing instead', 'will there be girls?']
+                full_response = random.choice(hang_responses)
+            if 'car' in received_message:
+                car_response = ['i used to have a car, but i crashed it on the information highway', 'bro i\'m a biker', 'i actually have a turbo in my transmission', 'i can only drive automatics']
+                full_response = random.choice(car_response)
 
-        if 'hang' in received_message:
-            hang_responses = ['jazz game', 'effing snales', 'gimme 20', 'i\'ll be there in an hour and a half', 'nah i can\'t hang i have weener stuff to be doing instead', 'will there be girls?']
-            full_response = random.choice(hang_responses)
-        if 'car' in received_message:
-            car_response = ['i used to have a car, but i crashed it on the information highway', 'bro i\'m a biker', 'i actually have a turbo in my transmission', 'i can only drive automatics']
-            full_response = random.choice(car_response)
+        if incl_tag:
+            return tagged_user + full_response
+        else:
+            return full_response
 
-    if incl_tag:
-        return tagged_user + full_response
-    else:
-        return full_response
+    elif message_attachment:
+        image_responses = ['dear lord', 'oh no', 'uh oh', 'this is literally me', 'don\'t ever let me catch you sending this again']
+        respond_or_not = [0, 1, 2, 3, 4, 5, 6]
+        will_respond = random.choice(respond_or_not)
+        if will_respond == 2:
+            return random.choice(image_responses)
 
-# TODO: try making a separate txt file for every member of the group and rewriting it every time someone's nickname is changed
 
 def add_nickname(name_to_nick) -> str:
     nick = name_to_nick
-    nicknames_read = open('nicknames.txt').readlines()
-    if name_to_nick in nicknames_read:
+    with open('nicknames/' + name_to_nick + '.txt', 'w') as nickname_file:
         nick = generate_nickname(name_to_nick)
-        with open('nicknames.txt', 'w') as nicknames_write:
-            to_write = ''
-            for line in nicknames_read:
-                if name_to_nick not in line:
-                    to_write += line + '\n'
-            to_write += '\n' + name_to_nick + '-' + nick
-            if to_write:
-                nicknames_write.write(to_write)
-    else:
-        nick = generate_nickname(name_to_nick)
-        with open('nicknames.txt', 'a') as nicknames_append:
-            to_append = '\n' + name_to_nick + '-' + nick
-            nicknames_append.write(to_append)
-    return nick
+        nickname_file.write(nick)
+        return nick
 
 
 def generate_nickname(name_to_nick) -> str:
@@ -110,31 +104,46 @@ def generate_nickname(name_to_nick) -> str:
 
 
 def get_nickname(sender) -> str:
-    with open('nicknames.txt', 'r') as nicknames_file:
-        for line in nicknames_file:
-            if sender in line:
-                return line.split('-', 1)[1]
+    with open('nicknames/' + sender + '.txt', 'r') as nickname_file:
+        nick_index = nickname_file.read()
+        if nick_index:
+            return nick_index
         else:
             return sender
 
 
+def update_message_index(message) -> str:
+    message_id = message['id']
+    with open('mostrecentmessage.txt', 'w') as message_index:
+        message_index.write(message_id)
+        return message_id
+
+
+def get_latest_message_id() -> str:
+    with open('mostrecentmessage.txt', 'r') as message_index:
+        id = message_index.read()
+        return id
+
+
 while True:
+    request_params['since_id'] = get_latest_message_id()
     response_page = requests.get('https://api.groupme.com/v3/groups/35396592/messages', params=request_params)
     if response_page.status_code == 200:
         response_messages = response_page.json()['response']['messages']
         for message in response_messages:
             message_sender = message['name']
             latest_message = message['text']
+            message_attachment = message['attachments']
             print(message['name'], ': ', message['text'])
 
             if message_sender in member_list:
-                to_send = analyze_message(message_sender, latest_message)
+                to_send = analyze_message(message_sender, latest_message, message_attachment)
                 if to_send:
                     # send response to the group
                     post_params = {'bot_id': '9ac5c52ec5efaee1bce225eb92', 'text': to_send}
                     requests.post('https://api.groupme.com/v3/bots/post', params=post_params)
-                    request_params['since_id'] = message['id']
                     break
+            request_params['since_id'] = update_message_index(message)
     time.sleep(5)
 
 
